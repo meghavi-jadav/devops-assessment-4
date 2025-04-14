2a8616f04f3d59ae25155a9b1b05304fd439de09
Meghavi Jadav <mjadav@parkar.digital>
2025-04-14 11:39:57 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,11 +1,12 @@
 def generate_fibonacci(n):
     if n <= 0:
         return "Please enter a positive integer."
-
+    
     sequence = [0, 1]
     while len(sequence) < n:
         sequence.append(sequence[-1] + sequence[-2])
     return sequence[:n]
+
 
 if __name__ == "__main__":
     number_of_terms = int(input("Enter the number of Fibonacci terms to generate: "))
